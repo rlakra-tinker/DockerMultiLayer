d89d05a9bb5a3275033e286545398e7738c3270b
John Standish <voldron@gmail.com>
2025-03-12 12:10:04 -0700

    feat: Set access logs

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,5 @@
 import os
-
+import json
 from multiprocessing import cpu_count
 
 # gunicorn -c gunicorn_config.py wsgi:app
@@ -17,9 +17,26 @@ workers = (cpu_count() * 2) + 1
 threads = pool_size - safety_net
 worker_class = 'gthread'
 proc_name = "gunicorn"
-default_proc_name = "gunicorn"
+# default_proc_name = "gunicorn"
 timeout = 60
-accesslog = '-'
-errorlog = '-'
+# accesslog = '-'
+# errorlog = '-'
 loglevel = 'debug'
 capture_output = True
+accesslog_var = os.getenv("ACCESS_LOG", "-")
+use_accesslog = accesslog_var or None
+accesslog = use_accesslog
+errorlog_var = os.getenv("ERROR_LOG", "-")
+use_errorlog = errorlog_var or None
+errorlog = use_errorlog
+
+log_data = {
+    "loglevel": loglevel,
+    "workers": workers,
+    "bind": bind,
+    "timeout": timeout,
+    "errorlog": errorlog,
+    "accesslog": accesslog,
+    # Additional, non-gunicorn variables
+}
+print(json.dumps(log_data))
